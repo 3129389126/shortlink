7b5321ba0202fc7061f6790ab032cbe636c157be
曾垂浩 <213253084@seu.edu.cn>
2026-09-01 22:56:38 +0800

    feat: 新增 hasUsername 用户名查重接口

--- admin/src/main/java/com/zch/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/zch/shortlink/admin/controller/UserController.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController
 
 /*
 * 用户管理控制层
