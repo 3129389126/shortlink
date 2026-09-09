2acd400123b05af4719e7c6e9ad3b437e22dec44
zch <3129389126@qq.com>
2026-09-09 17:30:55 +0800

    开发短链接后管系统用户退出登录功能

--- admin/src/main/java/com/zch/shortlink/admin/service/impl/UserServiceimpl.java
+++ admin/src/main/java/com/zch/shortlink/admin/service/impl/UserServiceimpl.java
@@ -147,8 +147,18 @@ public class UserServiceimpl extends ServiceImpl<UserMapper, UserDO> implements 
     public Boolean checkLogin(String username,String token){
         return stringRedisTemplate.opsForHash().get("login_" + username, token) != null;
     }
+
+    @Override
+    public void logout(String username, String token) {
+        if(checkLogin(username,token)){
+            stringRedisTemplate.delete("login_" + username);
+            return;
+        }
+        throw new ClientException("用户Token不存在或用户未登录");
+    }
+
 }
-        
+
 
 
 
